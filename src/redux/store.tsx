a56e6b190aa539b787d22a1e7a20e969f63baf0a
Poonam Minde <poonamminde789@gmail.com>
2024-03-21 18:36:13 +0530

    warning remove

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,14 +1,12 @@
 import { createStore } from "redux";
 
 const reducer = (state: any = {}, action: any) => {
-  console.log(state);
   switch (action.type) {
     case "load":
       return { ...action.data };
     default:
       return { ...state };
   }
-  console.log(state);
 };
 
 export const store = createStore(reducer);
